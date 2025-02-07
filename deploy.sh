3db9c581eb40dcb99a624a1b8cd899ce6378d778
G.SREEVADHANI <127740281+GSreevadhani@users.noreply.github.com>
2025-02-07 16:20:34 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-docker build -t maven-img .
-docker run -d -p 8070:8070 --name=maven-container maven-img
-docker push maven-img sreevadhani/maven-tag
+docker build -t maven-img -f docker/Dockerfile .
